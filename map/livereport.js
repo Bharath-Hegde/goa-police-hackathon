cc91bc8dab76bb26a312ef7b869018dd6967e1f3
Akhil Bansal <bansalakhil19@gmail.com>
2022-09-12 00:19:57 +0530

    make coordinate list dynamic. make 2 and fro clicks

--- map/livereport.js
+++ map/livereport.js
@@ -1,67 +1,25 @@
-// const arr = require('./report.js');
-var arr = [
-    {
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },
-    {
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },
-    {
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    },{
-        coordinates: [1,2],
-        ip:"123.123.123.123"
-    }
-]
-const liveReports = document.getElementById('live-reports');
+var arr=[];
+// setInterval(async () => {
+//     await fetch('http://localhost:4000/map').then(response => response.json()).then((data) => arr = data);
+//     createList(arr);
+// }, 500)
 
-console.log("ping")
-arr.forEach((element,index) => {
-    const div1 = document.createElement('div');
-    const div2 = document.createElement('div');
-    const div2_child1 = document.createElement('div');
-    div1.id=index;
-    div2_child1.innerHTML="Coordinates " + element.coordinates;
-    div2.appendChild(div2_child1);
+function createList(arr) {
+    arr?.forEach((element, index) => {
+        const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+        const div2_child1 = document.createElement('div');
+        div1.id = index;
+        div2_child1.innerHTML = "Coordinates " + element.coordinates;
+        div2.appendChild(div2_child1);
 
-    const li2= document.createElement('div');
-    li2.innerHTML= "IP Address "+element.ip
-    div2.appendChild(li2);
-    div1.appendChild(div2);
-    div1.classList.add('live-report-child')
-    
-    liveReports.appendChild(div1);
-});
+        const li2 = document.createElement('div');
+        li2.innerHTML = "IP Address " + element.ip
+        div2.appendChild(li2);
+        div1.appendChild(div2);
+        div1.classList.add('live-report-child')
+
+        liveReports.appendChild(div1);
+    });
+
+}
--- map/main.js
+++ map/main.js
@@ -2,6 +2,7 @@ import {useGeographic} from 'ol/proj';
 import Feature from 'ol/Feature';
 import Map from 'ol/Map';
 import Overlay from 'ol/Overlay';
+// import View from 'ol/View';
 import Point from 'ol/geom/Point';
 import TileJSON from 'ol/source/TileJSON';
 import VectorSource from 'ol/source/Vector';
@@ -17,7 +18,7 @@ import OSM from 'ol/source/OSM';
 
 
 
-let coordinateArrays = [[73.98018005767172, 15.42284259356], [73.99018005767172, 15.99284259356],[18.5167,73.9167],[73.8554,18.5196]] 
+let coordinateArrays = [] 
 let iconFeatures = []
 
 useGeographic();
@@ -117,7 +118,6 @@ map.on('pointermove', function (e) {
     var zoom = map.getView().getZoom();
     // var arr1 = feature.get('geometry').flatCoordinates[1]+(1/zoom)*1200*iconStyle.getImage().getScale();
     var arr1 = feature.get('geometry').flatCoordinates[1]+0.05;
-
     var arr0 = feature.get('geometry').flatCoordinates[0];
     console.log([arr0,arr1]);
     console.log(e.coordinate);
@@ -130,34 +130,80 @@ map.on('pointermove', function (e) {
     popover.show();}
   }
 });
+const liveReports = document.getElementById('live-reports');
 
 async function myFunction(){
-  var coordinateArrays;
+  // var coordinateArrays;
   const response = await fetch('http://127.0.0.1:4000/map/');
-  const coord = await response.json();
-  coordinateArrays=coord;
-  console.log(coord);
+  const jobj = await response.json();
+  coordinateArrays = jobj['coords'];
+  console.log(coordinateArrays);
+  console.log(jobj);
   var iconFeatures=[];
+  while (liveReports.firstChild) {
+    liveReports.removeChild(liveReports.lastChild);
+  }
   coordinateArrays.forEach(function(item,index){
     let temp = new Feature({
       geometry: new Point(item),
       num: index,
-      name: 'metadata',
+      name: "Sim Index "+jobj['fingroups'][index].toString(),
     })
     temp.setStyle(iconStyle);
     iconFeatures.push(temp);
+
+
+    
   });
+  const listReq=[];
+  for (let i = 0; i < coordinateArrays.length; i++) {
+    listReq.push({
+      coord: jobj.coords[i],
+      ip: jobj.ips[i],
+      fname: jobj.fnames[i],
+      similarityIndex:jobj.fingroups[i]
+    })
+    
+  }
+  listReq.forEach((element,index) => {
+    const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+        const div2_child1 = document.createElement('div');
+        div1.id = index;
+        div2_child1.innerHTML = "Coordinates: " + element.coord;
+        div2.appendChild(div2_child1);
+
+        const li2 = document.createElement('div');
+        li2.innerHTML = "IP Address: " + element.ip
+        div2.appendChild(li2);
+        const li3 = document.createElement('div');
+        li3.innerHTML = "File Name: " + element.fname
+        div2.appendChild(li3);
+        const li4 = document.createElement('div');
+        li4.innerHTML = "Similarity Index: " + element.similarityIndex
+        div2.appendChild(li4);
+        div1.appendChild(div2);
+        div1.classList.add('live-report-child')
+        
+        liveReports.appendChild(div1);
+
+        // div1.onclick = function(){
+        //   View.setCenter(element.coord);
+        //   console.log(element.coord);
+        // }
+  }
+  )
+  document.getElementById('malwareNumber').innerHTML=coordinateArrays.length;
   console.log(iconFeatures);    
   const vectorSource = new VectorSource({
     features: iconFeatures,
   });
-  // map.removeLayer(map.getLayers().getArray()[1]);
+  map.removeLayer(map.getLayers().getArray()[1]);
   const vectorLayer = new VectorLayer({
     source: vectorSource,
   });
   map.addLayer(vectorLayer);
 }
-
 var intervalId = setInterval(myFunction, 5000);
 
 
